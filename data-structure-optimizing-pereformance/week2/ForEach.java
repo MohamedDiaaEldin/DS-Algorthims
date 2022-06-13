9f6ef2f27a96cba167ddd9ac5354139ae998346d
MohamedDiaaEldin <mdiaan404@gmail.com>
2022-06-13 04:42:38 +0200

    recap second week regular and string methods - for each

---
+++ data-structure-optimizing-pereformance/week2/ForEach.java
@@ -0,0 +1,27 @@
+package week2 ;
+
+public class ForEach{
+
+    public static void main(String[] args) {
+        String text  = "mohamed" ;         
+        char[] name  = text.toCharArray() ;         
+        
+        /// while not replace char 
+        for (char c : name){
+            if (c == 'h'){
+                c = 'L' ; 
+            }            
+        }
+        System.out.println(name);
+
+        // will replace char 
+        for (int i=0 ; i<name.length ; i++){
+            if( name[i] == 'h'){
+                name[i] = 'L' ; 
+            }
+        }
+        
+        System.out.println(name);
+        System.out.println(new String(name));
+    }
+}
